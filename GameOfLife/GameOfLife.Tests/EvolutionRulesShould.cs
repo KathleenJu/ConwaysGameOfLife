6fd63c6e820c7d63b904ef02ee67d5ec3ba6564d
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-08-10 10:33:40 +1200

    added the first rule

---
+++ GameOfLife/GameOfLife.Tests/EvolutionRulesShould.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace GameOfLife.Tests
+{
+    public class EvolutionRulesShould
+    {
+        [Fact]
+        public void KillALiveCellThatHasFewerThanTwoNeighbours()
+        {
+            var rules = new EvolutionRules();
+            var livingCells = new List<Cell>();
+            var cellTarget = new Cell(1, 1);
+            livingCells.Add(cellTarget);
+            livingCells.Add(new Cell(0, 1));
+            var isKilled = rules.HasFewerThanTwoLiveNeighbours(livingCells, cellTarget);
+
+            Assert.True(isKilled);
+        }
+
+        [Fact]
+        public void NotKillALiveCellThatTwoOrMoreNeighbour()
+        {
+            var rules = new EvolutionRules();
+            var livingCells = new List<Cell>();
+            var cellTarget = new Cell(1, 1);
+            livingCells.Add(cellTarget);
+            livingCells.Add(new Cell(0, 1));
+            livingCells.Add(new Cell(1, 0));
+            livingCells.Add(new Cell(0, 2));
+            var isAlive = rules.HasFewerThanTwoLiveNeighbours(livingCells, cellTarget);
+
+            Assert.True(isAlive);
+        }
+    }
+}
--- GameOfLife/GameOfLife.Tests/GridShould.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GameOfLife.Tests
-{
-    public class GridShould
-    {
-        
-    }
-}
