9a72e849182978d8095f96f32f80c3cc42285356
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-08-10 10:07:29 +1200

    cell and grid

---
+++ GameOfLife/GameOfLife/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace GameOfLife
+{
+    public class Grid
+    {
+        public int GridHeight { get; }
+        public int GridWidth { get; }
+        private List<Cell> LivingCells;
+
+        public Grid(int gridHeight, int gridWidth)
+        {
+            GridHeight = gridHeight;
+            GridWidth = gridWidth;
+            LivingCells = new List<Cell>();
+        }
+    }
+}
