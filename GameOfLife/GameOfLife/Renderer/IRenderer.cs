6a651758f7afd82fd4631b908e85bfdf1f30035a
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-08-17 10:22:27 +1200

    renderer class

---
+++ GameOfLife/GameOfLife/Renderer/IRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GameOfLife
+{
+    public interface IRenderer
+    {
+        List<Cell> GetInitialStateOfGrid();
+        void RenderGrid();
+        void RenderMessage();
+    }
+}
