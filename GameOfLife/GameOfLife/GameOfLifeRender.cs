b53ff9b506ca8bc8e7ff0794f297ae515f83fd89
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-08-21 08:44:34 +1200

    refactored

---
+++ GameOfLife/GameOfLife/GameOfLifeRender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace GameOfLife
+{
+    public class GameOfLifeRender
+    {
+        private readonly Game Game; 
+        private readonly IRenderer Renderer;
+        
+        public GameOfLifeRender(Game game, IRenderer renderer)
+        {
+            Game = game;
+            Renderer = renderer;
+        }
+
+        public void StartGame()
+        {
+            Renderer.RenderMessage("Conway's Game of Life\n");
+            Renderer.RenderGrid(new Grid(30, 30));
+         
+            var height = Renderer.GetGridDimension("height");
+            var width = Renderer.GetGridDimension("width");
+            Game.SetGridSize(height, width);
+            
+            var initialCells = Renderer.GetInitialStateOfGrid();
+            Game.SetInitialStateOfGrid(initialCells);
+            Renderer.RenderGrid(Game.Grid);
+            
+            while (true)
+            {
+                Game.Evolve();
+                Renderer.RenderGrid(Game.Grid);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -9,23 +9,10 @@ namespace GameOfLife
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             var game = new Game();
-            var renderer = new ConsoleRenderer();
-            renderer.RenderMessage("Conway's Game of Life\n");
-            renderer.RenderGrid(new Grid(30,30));
-            var height = renderer.GetGridDimension("height");
-            var width = renderer.GetGridDimension("width");
-            game.SetGridSize(height, width);
-            var initialCells = renderer.GetInitialStateOfGrid();
-            game.SetInitialStateOfGrid(initialCells);
-            renderer.RenderGrid(game.Grid);
-            while (true)
-            {
-                game.Evolve();
-                renderer.RenderGrid(game.Grid);
-                Thread.Sleep(1000);
-            }
-
+            var consoleRenderer = new ConsoleRenderer();
+            var gameOfLife = new GameOfLifeRender(game, consoleRenderer);
             
+            gameOfLife.StartGame();
         }
     }
 }
